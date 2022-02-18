36c4cc88a9c3c28a0be5316d6d7759ee36a9a317
Carlos Ardila <carlos.ardila@appgate.com>
2022-02-18 13:21:12 -0500

    Part 2: First Version

--- main.py
+++ main.py
@@ -12,3 +12,12 @@ if __name__ == '__main__':
             count += 1
 
     print("Sonar Sweep - Part 1:", count)
+
+    count2 = 0
+    for i in range(3, len(depths)):
+        left = depths[i - 3] + depths[i - 2] + depths[i - 1]
+        right = depths[i - 2] + depths[i - 1] + depths[i]
+        if left < right:
+            count2 += 1
+
+    print("Sonar Sweep - Part 2:", count2)
